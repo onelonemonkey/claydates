a7faa2555b555331e87e7d8556030ca56203b4dd
White <Alan.White@franklintempleton.co.uk>
2018-02-20 12:12:54 +0000

    testing change and deploy

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -15,16 +15,9 @@ const TemplateWrapper = ({ children }) => (
       ]}
     />
     <Header />
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '0px 1.0875rem 1.45rem',
-        paddingTop: 0,
-      }}
-    >
+    <main>
       {children()}
-    </div>
+    </main>
   </div>
 )
 
