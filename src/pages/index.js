295e8ad380f6320e51162cc1ec28ce42649e1a4d
White <Alan.White@franklintempleton.co.uk>
2018-02-20 11:45:01 +0000

    added gatsby

---
+++ src/pages/index.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Claydates <small>Create and be social!</small></h1>
+
+    <section>
+      <h2>What are Claydates?</h2>
+      <p>Claydates is a social endeavour based around making things out of clay. An extremely therapeutic passtime we highly recommend you try with your friends, kids or even your enemies!</p>
+    </section>
+
+    <section>
+      <h2>How to have a Claydate</h2>
+      <p>It's really quite simple...</p>
+      <ol>
+        <li>Buy lots of clay</li>
+        <li>Get hold of other participants</li>
+        <li>Create and be social!</li>
+      </ol>
+    </section>
+
+    <section>
+      <h2>What should I make?</h2>
+      <p>Whatever comes to mind!</p>
+    </section>
+
+  </div>
+)
+
+export default IndexPage
