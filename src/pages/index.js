0960507b66195fb553465704280eb33a60aca48b
White <Alan.White@franklintempleton.co.uk>
2018-02-20 14:35:49 +0000

    Updated typeface

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,9 +5,9 @@ const IndexPage = () => (
   <div>
     {/* <h1>Claydates <small>Create and be social!</small></h1> */}
 
-    <section>
+    <section class="intro">
       <h1>Claydates</h1>
-      <p>Claydates is a social endeavour based around making things out of clay. An extremely therapeutic passtime we highly recommend you try with your friends, kids or even your enemies!</p>
+      <p>Claydates is a social endeavour based around <em>ditching technology</em>, <em>hanging out</em> and <em>making things out of clay</em>. An extremely therapeutic passtime we highly recommend you try with your friends, kids or even your enemies!</p>
     </section>
 
   </div>
