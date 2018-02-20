ace5d30ebe70159ac4cfa431035d97930db67c88
White <Alan.White@franklintempleton.co.uk>
2018-02-20 16:35:18 +0000

    added clayman

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import clayman from "./clayman.png";
 
 const IndexPage = () => (
   <div>
@@ -7,7 +8,8 @@ const IndexPage = () => (
 
     <section class="intro">
       <h1>Claydates</h1>
-      <p>Claydates is a social endeavour based around <em>ditching technology</em>, <em>hanging out</em> and <em>making things out of clay</em>. An extremely therapeutic passtime we highly recommend you try with your friends, kids or even your enemies!</p>
+      <p>Claydates is a social endeavour based around <em>ditching technology</em>, <em>hanging&nbsp;out</em> and <em>making things out of clay</em>. An extremely therapeutic passtime we highly recommend you try with your friends, kids or even your enemies!</p>
+      <img src={clayman} alt="Clayman" />
     </section>
 
   </div>
