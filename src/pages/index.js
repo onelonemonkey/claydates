7d723d633e203269cb780e497b017a71be4ecc9f
White <Alan.White@franklintempleton.co.uk>
2018-02-20 16:56:41 +0000

    type change

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,7 @@ const IndexPage = () => (
 
     <section class="intro">
       <h1>Claydates</h1>
-      <p>Claydates is a social endeavour based around <em>ditching technology</em>, <em>hanging&nbsp;out</em> and <em>making things out of clay</em>. An extremely therapeutic passtime we highly recommend you try with your friends, kids or even your enemies!</p>
+      <p>Claydates is a social endeavour based around <em>ditching technology</em>, <em>hanging&nbsp;out</em> and <em>making things out of clay</em>. An extremely therapeutic passtime which we highly recommend you try with your friends, kids or even your enemies!</p>
       <img src={clayman} alt="Clayman" />
     </section>
 
