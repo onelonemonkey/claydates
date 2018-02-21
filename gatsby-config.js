d4bb82790678d87fe7debf1f3b43e9c070abb37d
White <Alan.White@franklintempleton.co.uk>
2018-02-21 10:03:51 +0000

    connecting to datocms

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,13 @@ module.exports = {
   siteMetadata: {
     title: 'Gatsby Default Starter',
   },
-  plugins: ['gatsby-plugin-react-helmet'],
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: `gatsby-source-datocms`,
+      options: {
+        apiToken: `745a8c7277de25170bb4e44640c62b`,
+      },
+    },
+  ],
 };
