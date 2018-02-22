79f2d2b46f2d6ca8eaeedeab8abbe374e6c503bf
White <Alan.White@franklintempleton.co.uk>
2018-02-22 17:24:23 +0000

    removed datocms config

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,15 +4,6 @@ module.exports = {
   },
   plugins: [
     'gatsby-plugin-react-helmet',
-<<<<<<< HEAD
-    {
-      resolve: `gatsby-source-datocms`,
-      options: {
-        apiToken: `745a8c7277de25170bb4e44640c62b`,
-      },
-    },
-=======
     'gatsby-plugin-sass'
->>>>>>> abfaa81f0b59dfe053975d9fbfd90ffa0ef40d80
   ],
 };
